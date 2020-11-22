fa5d2ef670c0a755fe092e15afc6291b89e502e6
Milan Babuljak <milanbabuljak8@gmail.com>
2020-11-23 00:04:46 +0100

    +

---
+++ LibrarySystem/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terminal.Gui;
+
+namespace LibrarySystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Application.Init();
+
+            var top = Application.Top;
+
+            Console.Title = "Kniznicny System";
+
+            var win = new Window()
+            {
+                Title = "Library System",
+                Width = Dim.Fill(),
+                Height = Dim.Fill()
+            };
+            win.Add(
+                new Label(Console.WindowWidth/2-8,0,"Library System"),
+                new Button(Console.WindowWidth/2-9,2,"Search Books")
+                
+                
+                
+            );
+
+
+
+
+            top.Add(win);
+
+            var menuBar = new MenuBar(new MenuBarItem[]
+                {
+                    new MenuBarItem("File", new MenuItem[]
+                    {
+                        new MenuItem("New project", "", () => {}),
+                        new MenuItem("Open project", "", () => { }),
+                        new MenuItem("Save project", "", () => { }),
+                        new MenuItem("Export subtitles", "", () => { }),
+                    }),
+                    new MenuBarItem("Settings", new MenuItem[]
+                    {
+
+                    }),
+                    new MenuBarItem("About", new MenuItem[]
+                    {
+
+                    }),
+                }
+            );
+
+            top.Add(menuBar);
+            Application.Run();
+        }
+    }
+}
