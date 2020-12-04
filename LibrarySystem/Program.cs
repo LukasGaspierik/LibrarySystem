672e324326f09f7d029ea1a03a4aaea705f8195e
Lukáš Gašpierik <Luk Gapierik@LAPTOP-GBRVOBEH>
2020-12-04 01:00:35 +0100

    Sme Borcii

--- LibrarySystem/Program.cs
+++ LibrarySystem/Program.cs
@@ -4,22 +4,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Terminal.Gui;
+using System.IO;
 
 namespace LibrarySystem
 {
     class Program
     {
+        private static Toplevel top;
         static void Main(string[] args)
         {
             Application.Init();
-
-            var top = Application.Top;
-            
-
+            top = Application.Top; 
             Console.Title = "Kniznicny System";
             Colors.Base.Normal = Application.Driver.MakeAttribute(Color.White, Color.Black);
-
-
 
             var win = new Window()
             {
@@ -28,210 +25,78 @@ namespace LibrarySystem
                 Height = Dim.Fill()
             };
 
-            var mat = new Window()
+
+            OpenDialog opendialog = new OpenDialog("Open Bookfile", "Open Bookfile");
+
+            void OnClickEvent()
             {
-                Title = "Matematika",
-                Width = Dim.Fill(),
-                Height = Dim.Fill(),
-                Visible = true,
-
-            };
-
-            var Dej = new Window()
-            {
-                Title = "Dejepis",
-                Width = Dim.Fill(),
-                Height = Dim.Fill(),
-                Visible = true,
-
-            };
-
-            var Ang = new Window()
-            {
-                Title = "Angličtina",
-                Width = Dim.Fill(),
-                Height = Dim.Fill(),
-                Visible = true,
-
-            };
-
-            var Nej = new Window()
-            {
-                Title = "Nemčina",
-                Width = Dim.Fill(),
-                Height = Dim.Fill(),
-                Visible = true,
-                
-
-            };
-
-            var Pro = new Window()
-            {
-                Title = "Programovanie",
-                Width = Dim.Fill(),
-                Height = Dim.Fill(),
-                Visible = true,
-
-            };
-
-            var Ele = new Window()
-            {
-                Title = "Elektrotechnika",
-                Width = Dim.Fill(),
-                Height = Dim.Fill(),
-                Visible = true,
-
-            };
-
-            void TurnOnMath()
-            {
-
-                top.Add(mat);
-                top.Redraw(new Rect(0, 0, Console.WindowWidth, Console.WindowHeight));
+                Button btnCLick = new Button(50, 19, "Read");
+                opendialog.Add(btnCLick);
+                btnCLick.Clicked += onSelectBook;
+                Window temp = new Window();
+                temp.Add(opendialog);
+                top.Add(temp);
 
             }
-            void TurnOnDej()
+
+            void onSelectBook()
             {
+                string[] readText = File.ReadAllLines(opendialog.FilePath.ToString());
+                string nazov = readText[0];
+                string autor = readText[1];
+                string knihydokopy = readText[2];
+                string pozkniny = readText[3];
+                string knihykdisp = readText[4];
+                string pocetstran = readText[5];
 
-                top.Add(Dej);
-                top.Redraw(new Rect(0, 0, Console.WindowWidth, Console.WindowHeight));
-            }
-            void TurnOnAng()
-            {
+                var preview = new Window()
+                {
+                    Title = nazov,
+                    Width = Dim.Fill(),
+                    Height = Dim.Fill(),
+                    Visible = true,
+                     
+                };
 
-                top.Add(Ang);
-                top.Redraw(new Rect(0, 0, Console.WindowWidth, Console.WindowHeight));
-
-            }
-            void TurnOnNej()
-            {
-
-                top.Add(Nej);
-                top.Redraw(new Rect(0, 0, Console.WindowWidth, Console.WindowHeight));
-
-            }
-            void TurnOnPro()
-            {
-
-                top.Add(Pro);
-                top.Redraw(new Rect(0, 0, Console.WindowWidth, Console.WindowHeight));
-
-            }
-            void TurnOnEle()
-            {
-
-                top.Add(Ele);
-                top.Redraw(new Rect(0, 0, Console.WindowWidth, Console.WindowHeight));
-
-            }
-            
-            int consoleCenter = Console.WindowWidth / 2;
-
-            var btnMat = new Button(Console.WindowWidth / 2 - 7, 10, "Matematika");
-            btnMat.Clicked += TurnOnMath;
-            var btnDej = new Button(Console.WindowWidth / 2 - 6, 12, "Dejepis");
-            btnDej.Clicked += TurnOnDej;
-            var btnAng = new Button(Console.WindowWidth / 2 - 7, 14, "Angličtina");
-            btnAng.Clicked += TurnOnAng;
-            var btnNej = new Button(Console.WindowWidth / 2 - 6, 16, "Nemčina");
-            btnNej.Clicked += TurnOnNej;
-            var btnPro = new Button(Console.WindowWidth / 2 - 9, 18, "Programovanie");
-            btnPro.Clicked += TurnOnPro;
-            var btnEle = new Button(Console.WindowWidth / 2 - 10, 20, "Elektrotechnika");
-            btnEle.Clicked += TurnOnEle;
-            win.Add(
-            //...
-            btnAng,btnMat,btnDej,btnNej,btnPro,btnEle,
-            //...
-
-            new Label(consoleCenter - 37, 0, @"  _      _ _                             _____           _                 "),
-            new Label(consoleCenter - 37, 1, @" | |    (_) |                           / ____|         | |                "),
-            new Label(consoleCenter - 37, 2, @" | |     _| |__  _ __ __ _ _ __ _   _  | (___  _   _ ___| |_ ___ _ __ ___  "),
-            new Label(consoleCenter - 37, 3, @" | |    | | '_ \| '__/ _` | '__| | | |  \___ \| | | / __| __/ _ \ '_ ` _ \ "),
-            new Label(consoleCenter - 37, 4, @" | |____| | |_) | | | (_| | |  | |_| |  ____) | |_| \__ \ ||  __/ | | | | |"),
-            new Label(consoleCenter - 37, 5, @" |______|_|_.__/|_|  \__,_|_|   \__, | |_____/ \__, |___/\__\___|_| |_| |_|"),
-            new Label(consoleCenter - 37, 6, @"                                 __/ |          __/ |                      "),
-            new Label(consoleCenter - 37, 7, @"                                |___/          |___/                       ")
-
-
-
-            );
-            mat.Add(
-            new Label(0,0, "Autor"),
-            new Label(0,1, "Počet Kníh"),
-            new Label(0,2, "Počet Strán"),
-            new Label(0,3, "Obsah"),
-            new Label(0,4, "Požičané"),
-            new Label(0,5, "Dostupné")
-
-
-            );
-            Dej.Add(
-            new Label(0, 0, "Autor"),
-            new Label(0, 1, "Počet Kníh"),
-            new Label(0, 2, "Počet Strán"),
-            new Label(0, 3, "Obsah"),
-            new Label(0, 4, "Požičané"),
-            new Label(0, 5, "Dostupné")
-
-             
-            );
-            Ang.Add(
-            new Label(0, 0, "Autor"),
-            new Label(0, 1, "Počet Kníh"),
-            new Label(0, 2, "Počet Strán"),
-            new Label(0, 3, "Obsah"),
-            new Label(0, 4, "Požičané"),
-            new Label(0, 5, "Dostupné")
-
-
-
-            );
-            Nej.Add(
-            new Label(0, 0, "Autor"),
-            new Label(0, 1, "Počet Kníh"),
-            new Label(0, 2, "Počet Strán"),
-            new Label(0, 3, "Obsah"),
-            new Label(0, 4, "Požičané"),
-            new Label(0, 5, "Dostupné")
-
-            
-            );
-            Pro.Add(
-            new Label(0, 0, "Autor"),
-            new Label(0, 1, "Počet Kníh"),
-            new Label(0, 2, "Počet Strán"),
-            new Label(0, 3, "Obsah"),
-            new Label(0, 4, "Požičané"),
-            new Label(0, 5, "Dostupné")
-
-
-            );
-            Ele.Add(
-            new Label(0, 0, "Autor"),
-            new Label(0, 1, "Počet Kníh"),
-            new Label(0, 2, "Počet Strán"),
-            new Label(0, 3, "Obsah"),
-            new Label(0, 4, "Požičané"),
-            new Label(0, 5, "Dostupné")
-
+                preview.Add(
+                    new Label(0, 0, "Nazov: " + nazov),
+                    new Label(0, 1, "Author: " + autor),
+                    new Label(0, 2, "Knihy do kopy: " + knihydokopy),
+                    new Label(0, 3, "Požičané kniny: " + pozkniny),
+                    new Label(0, 4, "Knihy k dispozícii: " + knihykdisp),
+                    new Label(0, 5, "Počet strán: " + pocetstran)
                 );
 
 
+                top.Add(preview);
+
+            }
 
 
+
+
+            int consoleCenter = Console.WindowWidth / 2;
+
+            var btnFindBook = new Button(Console.WindowWidth / 2 - 7, 10, "Najdi knihu");
+            btnFindBook.Clicked += OnClickEvent;
+
+
+            win.Add(btnFindBook);
+                win.Add(
+                    new Label(consoleCenter - 37, 0, @"  _      _ _                             _____           _                 "),
+                    new Label(consoleCenter - 37, 1, @" | |    (_) |                           / ____|         | |                "),
+                    new Label(consoleCenter - 37, 2, @" | |     _| |__  _ __ __ _ _ __ _   _  | (___  _   _ ___| |_ ___ _ __ ___  "),
+                    new Label(consoleCenter - 37, 3, @" | |    | | '_ \| '__/ _` | '__| | | |  \___ \| | | / __| __/ _ \ '_ ` _ \ "),
+                    new Label(consoleCenter - 37, 4, @" | |____| | |_) | | | (_| | |  | |_| |  ____) | |_| \__ \ ||  __/ | | | | |"),
+                    new Label(consoleCenter - 37, 5, @" |______|_|_.__/|_|  \__,_|_|   \__, | |_____/ \__, |___/\__\___|_| |_| |_|"),
+                    new Label(consoleCenter - 37, 6, @"                                 __/ |          __/ |                      "),
+                    new Label(consoleCenter - 37, 7, @"                                |___/          |___/                       ")
+                    ); 
+                
             top.Add(win);
 
-
-            Console.CursorVisible = false;
-            top.Add(win);
-            Console.CursorVisible = false;
-
-            var opendialog = new OpenDialog("Open Save", "Open up your Save");
-            var savedialog = new OpenDialog("Continue Chapter", "Open up your save!");
-            Console.CursorVisible = false;
-
-            var menuBar = new MenuBar(new MenuBarItem[] {
+            /*
+             * var menuBar = new MenuBar(new MenuBarItem[] {
                 new MenuBarItem("File", new MenuItem[] {
                     new MenuItem ("SKOLA", "", () => Application.Run (opendialog)),
                     new MenuItem ("EPIKA", "", () => Application.Run(savedialog)),
@@ -257,6 +122,8 @@ namespace LibrarySystem
             }
             );
             top.Add(menuBar);
+            */
+
             Application.Run();
 
         }
